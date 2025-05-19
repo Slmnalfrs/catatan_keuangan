89d5626a3ee567e61324303db7e5c90c2bb434f6
Salmanalfarisi <salmanalfarisi120603@gmail.com>
2025-05-19 18:55:13 +0700

    menyambungkan ke database di bagian main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'pages/home_page.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+const String supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVvY3d6bmx2a2RoZWh4aGltaHpkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc2NDI5NTAsImV4cCI6MjA2MzIxODk1MH0.ZalkBAJyeXfO-dlyF-TpmHUCYdYnU2xzm58QXtES7xk';
+
+Future<void> main() async {
+  await Supabase.initialize( 
+    url: 'https://uocwznlvkdhehxhimhzd.supabase.co',
+    anonKey: supabaseKey,
+  );
   runApp(CatatanKeuanganApp());
 }
 
